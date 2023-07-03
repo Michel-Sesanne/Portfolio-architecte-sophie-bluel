ff039ec9e9380b0a89afb93b7fcd52b6c431d775
Michel Sesanne <msesanne@gmail.com>
2023-07-03 06:00:59 +0200

    Codage de la page de connexion

---
+++ FrontEnd/login.js
@@ -0,0 +1,31 @@
+const formulaireLogIn = document.querySelector("form");
+
+formulaireLogIn.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const logIn = {
+    email: event.target.querySelector("#email").value,
+    password: event.target.querySelector("#password").value,
+  };
+  const chargeUtile = JSON.stringify(logIn);
+
+  fetch("http://localhost:5678/api/users/logIn", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" }, 
+    body: chargeUtile
+  })
+  .then(async (response) => {
+    if (response.ok) {
+      const data = await response.json();
+      const userToken = data.token;
+      localStorage.setItem("token", userToken);
+      
+      window.location.href = "index.html";
+    }else{
+      alert("Erreur dans l’identifiant ou le mot de passe");
+    }
+  })
+  .catch(error => {
+    alert(error);
+  });
+});
