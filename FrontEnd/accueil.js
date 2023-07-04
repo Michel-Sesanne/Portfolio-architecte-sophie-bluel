63cd540e6cf6e7f4753d471c2e58138ea6db9041
Michel Sesanne <msesanne@gmail.com>
2023-07-04 14:49:51 +0200

    Affichage du mode édition

--- FrontEnd/accueil.js
+++ FrontEnd/accueil.js
@@ -5,7 +5,7 @@
         const travaux = await reponse.json();
     
         //Ajout des travaux à la galerie
-        const galerieElement = document.querySelector('.gallery');
+        const galerieElement = document.querySelector(".gallery");
         afficherProjets(travaux, galerieElement);
     
         //Création d'un tableau des catégories sans doublon
@@ -19,11 +19,11 @@
         //Réalisation d'un filtre des travaux par catégorie de projet
         for(let i = 0; i < categories.length; i++) {
             //Ajout d'un bouton par catégorie
-            const categorieElement = document.createElement('button');
+            const categorieElement = document.createElement("button");
             categorieElement.innerText = categories[i];
-            categorieElement.setAttribute('class', `filter-btn btn-${i}`);
+            categorieElement.className = `filter-btn btn-${i}`;
     
-            const filterElement = document.querySelector('.filter');
+            const filterElement = document.querySelector(".filter");
             filterElement.appendChild(categorieElement);
     
             //Implementation de la fonction de filtrage des travaux
@@ -42,14 +42,14 @@
             });
         }
         //Mise en évidence du filtre actif
-        const filtres = document.querySelectorAll('button');
-        filtres[0].classList.add('active-filter'); // "Tous" par défaut
+        const filtres = document.querySelectorAll("button");
+        filtres[0].classList.add("active-filter"); // "Tous" par défaut
         for (const filtre of filtres) {
             filtre.addEventListener("click", () => {
                 for (const btn of filtres) {
-                    btn.classList.remove('active-filter');
+                    btn.classList.remove("active-filter");
                 }
-                filtre.classList.add('active-filter');
+                filtre.classList.add("active-filter");
             });
         };        
     } catch (error) {
@@ -59,14 +59,14 @@
 
 function afficherProjets(liste, galerie) {    
     for (let i = 0; i < liste.length; i++) {
-        const projet = document.createElement('figure');
+        const projet = document.createElement("figure");
         galerie.appendChild(projet);
 
-        const imageElement = document.createElement('img');
+        const imageElement = document.createElement("img");
         imageElement.src = liste[i].imageUrl;
         imageElement.alt = liste[i].title;
 
-        const titreElement = document.createElement('figcaption');
+        const titreElement = document.createElement("figcaption");
         titreElement.innerText = liste[i].title;
 
         projet.appendChild(imageElement);
@@ -74,3 +74,66 @@ function afficherProjets(liste, galerie) {
     };
 }
 
+//Changement d'apparence après authentification
+if(window.localStorage.getItem("token")) {
+    //login devient logout
+    const logInOut = document.querySelector(".logInOut");
+    logInOut.innerText = "logout";
+    logInOut.addEventListener("click", () => {
+        window.localStorage.removeItem("token");
+    });
+
+    //Ajout barre transversale supérieure
+    const barreAdmin = document.createElement("div");
+    barreAdmin.className = "admin-bar";
+    const bodyElement = document.querySelector("body");
+    const headerElement = document.querySelector("header");
+    headerElement.className = "header";
+    bodyElement.insertBefore(barreAdmin, headerElement);
+        //son contenu (le mode edition)
+    const iconeEdition = document.createElement("i");
+    const texteEdition = document.createElement("p");
+    const boutonEdition = document.createElement("button");
+    iconeEdition.className = "fa-regular fa-pen-to-square white";
+    texteEdition.className = "white";
+    boutonEdition.id = "edition-btn";
+    texteEdition.innerText = "Mode édition";
+    boutonEdition.textContent = "publier les changements";
+    barreAdmin.appendChild(iconeEdition);
+    barreAdmin.appendChild(texteEdition);
+    barreAdmin.appendChild(boutonEdition);
+
+    //Suppression de l'affichage du filtre
+    const filtre = document.querySelector(".filter");
+    filtre.classList.add("undisplay");
+
+    //Ajout visuel option "modifier"
+    const modifierElement = document.createElement("div");    
+    const iconeModifier = document.createElement("i");
+    const texteModifier = document.createElement("p");
+    modifierElement.className = "modifier";
+    modifierElement.setAttribute("onclick", "modifierIntro()");
+    iconeModifier.className = "fa-regular fa-pen-to-square black";
+    texteModifier.innerText = "modifier";
+    modifierElement.appendChild(iconeModifier);
+    modifierElement.appendChild(texteModifier);
+
+
+        //sous la photo d'introduction
+    const modifierIntro = document.createElement("div");
+    modifierIntro.appendChild(modifierElement);
+    const figureIntro = document.querySelector("figure");
+    figureIntro.appendChild(modifierIntro);
+
+        //à côté du titre "Mes projets"
+    const titreProjetsElement = document.createElement("div");
+    titreProjetsElement.className = "titreProjets";
+    const portfolioElement = document.getElementById("portfolio");
+    const titreMesProjets = document.querySelector("#portfolio h2");
+    portfolioElement.insertBefore(titreProjetsElement, titreMesProjets);    
+    const modifierElementBis = modifierElement.cloneNode(true);
+    modifierElementBis.setAttribute("onclick", "modifierProjets()");
+    titreProjetsElement.appendChild(titreMesProjets);    
+    titreProjetsElement.appendChild(modifierElementBis);   
+    
+};
